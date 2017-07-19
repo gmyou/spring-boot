bd0b200298f527001c98fc8cde04f7a2d96fb059
gmyou <gmyou71@gmail.com>
2017-07-20 04:09:00 +0900

    save

--- api/src/main/java/com/example/member/RestApiController.java
+++ api/src/main/java/com/example/member/RestApiController.java
@@ -1,17 +1,18 @@
 package com.example.member;
 
+import java.util.HashMap;
 import java.util.Map;
+
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.member.domain.Member;
 import com.example.member.repository.MemberRepository;
 
 @RestController
@@ -22,10 +23,30 @@ public class RestApiController {
 	MemberRepository memberRepository;
 	
 	@RequestMapping(value = "/member/", method = RequestMethod.POST)
-    public ResponseEntity<?> createUser(@RequestBody Map<String, Object> json) {
+
+    public Map<String, Object> createUser(@RequestBody Map<String, Object> json) {
 		logger.info("Creating Member : {}", json);
  
-		return new ResponseEntity<String>(json.toString(), HttpStatus.CREATED);
+		Member member = new Member();
+		member.setUsername(json.get("username").toString());
+		member.setAccount(json.get("account").toString());
+		member.setPassword(json.get("password").toString());
+		
+		logger.info("Mapped Member : {}", member);
+
+		Map<String, Object> result = new HashMap<String, Object>();
+
+		try {
+			memberRepository.save(member);
+			
+			result.put("result", "succ");
+			result.put("message", "-");
+		} catch(Exception e) {
+			result.put("result", "fail");
+			result.put("message", e.getMessage());
+		}
+		
+		return result;
     }
 	
 }
--- api/src/main/java/com/example/member/domain/Member.java
+++ api/src/main/java/com/example/member/domain/Member.java
@@ -16,31 +16,37 @@ public class Member {
 	private String password;
 	@Column
 	private String account;
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getAccount() {
 		return account;
 	}
+
 	public void setAccount(String account) {
 		this.account = account;
 	}
-	
-	
+
 }
